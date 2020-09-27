22e5cecf6581a359f77e91aee7ddcc7c8f73e3ac
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-09-27 21:39:21 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -89,6 +89,7 @@ programmer_hash
 end
 
 def changing_alan
+	
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -106,7 +107,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+programmer_hash[:alan_kay][:known_for] = "GUI"
+programmer_hash
      
 end
 
