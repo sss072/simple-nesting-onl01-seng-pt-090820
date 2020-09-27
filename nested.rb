ad829ec82cc7aeacef0b762e90d4183254131df1
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-09-27 21:33:09 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
         }
      }
 
-
+  programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,9 +37,12 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+programmer_hash[:alan_kay][:known_for]
+
 end
 
 def dennis_ritchies_language
+	programmer_hash[:dennis_ritchie]
 	programmer_hash = 
  		{
         :grace_hopper => {
